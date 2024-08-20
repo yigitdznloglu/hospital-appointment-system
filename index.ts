9ef0ceace0a9093055eb03975eb64612d3dd3e6b
Yigit Duzenlioglu <yigitduzenlioglu@icloud.com>
2024-08-19 20:52:53 -0500

    initial server setup in index.ts

---
+++ index.ts
@@ -0,0 +1,36 @@
+import express, {Request, Response} from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+// Load environment variables
+dotenv.config();
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// DB Connection
+const mongoURI = process.env.MONGO_URI;
+
+if (!mongoURI) {
+    throw new Error('MONGO_URI is not defined in the environment variables.');
+}
+
+mongoose.connect(mongoURI)
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch(err => {
+        console.error('Failed to connect to MongoDB', err);
+    });
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hospital Appointment System API');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
